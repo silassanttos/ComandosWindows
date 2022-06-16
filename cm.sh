7c8b33efeda9d29eda109b6017fced152a886353
Silas Dos Santos Sousa <69328711+silassanttos@users.noreply.github.com>
2022-06-16 16:33:45 -0300

    Create cm.sh

---
+++ cm.sh
@@ -0,0 +1,274 @@
+Adicionar novo hardware ⇒ hdwwiz.cpl
+Adicionar/Remover programas ⇒ appwiz.cpl
+Adicionar/Remover Programas: appwiz.cpl
+Administrador de Fonte de Dados ODBC: odbccp32.cpl
+Administrador de fontes de dados ODBC ⇒ odbccp32.cpl
+Adobe Acrobat (Executar se instalado ) ⇒ acrobat
+Adobe Designer (Executar se instalado ) ⇒ acrodist
+Adobe Distiller (Executar se instalado ) ⇒ acrodist
+Adobe ImageReady (Executar se instalado ) ⇒ imageready
+Adobe Photoshop (Executar se instalado ) ⇒ photoshop
+Ajuda e suporte ⇒ helpctr
+Ajustar Monitor ⇒ perfmon
+Ajustar Monitor ⇒ perfmon.msc
+Armazenamento removível ⇒ ntmsmgr.msc
+Armazenamento Removível: ntmsmgr.msc
+Arquivos compartilhados ⇒ fsmgmt.msc
+Assistente de acessibilidade ⇒ accwiz
+Assistente de Cameras e Scanners ⇒ sticpl.cpl
+Assistente de controles ⇒ access.cpl
+Assistente para Adicionar Hardware: hdwwiz.cpl
+Assistente para Configuração de Rede: netsetup.cpl
+Assistente para transferência BlueTooth ⇒ fsquirt
+Assistente para Transferência de Arquivos Bluetooth: fsquirt
+Assistente para transferência de arquivos e informações ⇒ migwiz
+Atualizações automáticas ⇒ wuaucpl.cpl
+Atualizações Automáticas: wuaucpl.cpl
+Bate-Papo: winchat
+Bloco de notas ⇒ notepad
+Busca rápida ⇒ findfast.cpl
+Calculadora ⇒ calc
+Catalogo Telefonico ⇒ rasphone
+Central de Segurança do Windows: wscui.cpl
+Centro de segurança do Windows ⇒ wscui.cpl
+Certificados: certmgr.msc
+Check Disk Utility ⇒ chkdsk
+Check Disk: chkdsk
+Cliente de Telnet: telnet
+Compartilhamentos DDE: ddeshare
+Conectar a Desktop Remoto atravhás de um Host ou IP ⇒ mstsc
+Conectar-se ao Site do Windows Update: wupdmgr
+Conexão de Área de Trabalho Remota: mstsc
+Conexões de rede ⇒ control netconnections
+Conexões de rede ⇒ ncpa.cpl
+Conexões de Rede: control netconnections
+Conexões de Rede: ncpa.cpl
+Configuração de protocolo de Internet (Deletar informações de DNS ) ⇒ ipconfi/flushdns
+Configuração de protocolo de Internet (Todas as conexões ) ⇒ ipconfig /release
+Configuração de protocolo de Internet( Visualizar tudo) ⇒ ipconfig /all
+Configuração de protocolo de Internet(Visualizar DNS ) ⇒ ipconfig /displaydns
+Configuração do IP (Atualiza todas as concessões DHCP e registra novamente nomes DNS): ipconfig /registerdns
+Configuração do IP (Depura a Cache DNS Resolver): ipconfig /flushdns
+Configuração do IP (Exibe informações completas da Configuração da Conexão): ipconfig /all
+Configuração do IP (Exibe o conteúdo da Cache DNS Resolver): ipconfig /displaydns
+Configuração do IP (Exibe todas as identificações de classe DHCP permitidas para o adaptador): ipconfig /showclassid
+Configuração do IP (Libera o endereço IP para o adaptador especificado): ipconfig /release
+Configuração do IP (Modifica a identificação de classe DHCP): ipconfig /setclassid
+Configuração do IP (Renova o endereço IP para o adaptador especificado): ipconfig /renew
+Configurações de segurança ⇒ secpol.msc
+Configurações Locais de Segurança: secpol.msc
+Configurações regionais ⇒ intl.cpl
+Conjunto de Directivas Resultantes (XP Prof): rsop.msc
+Consulta de diretivas resultantes ⇒ rsop.msc
+Contas de Usuário: control userpasswords2
+Contas de Usuário: nusrmgr.cpl
+Controladores de Jogo: joy.cpl
+Controle para Jogos ⇒ joy.cpl
+DDE Shares ⇒ ddeshare
+Definições locais de segurança: secpol.msc
+Desempenho: perfmon
+Desempenho: perfmon.msc
+Desfragmentador de Disco: dfrg.msc
+Desfragmentador de Disco: dfrg.msc
+Desfragmentador de Discos ⇒ dfrg.msc
+Desligar ⇒ shutdown
+Desligar o Windows: shutdown
+Direct X Control Panel (Se instalado )* ⇒ directx.cpl
+Direct X Troubleshooter ⇒ dxdiag
+Discador ⇒ dialer
+Dr. Watson System Troubleshooting Utility ⇒ drwtsn32
+Dr. Watson: drwtsn32
+Driver Verifier Utility ⇒ verifier
+Editor de Caracteres Particulares: eudcedit
+Editor de characteres particulares (algúem conhece isso?) ⇒ eudcedit
+Editor de Configuração do Sistema: sysedit
+Editor de Registros ⇒ regedit
+Editor de Registros ⇒ regedit32
+Editor do Registro: regedit
+Editor do Registro: regedit32
+Event Viewer ⇒ eventvwr.msc
+Fazer Logoff do Windows ⇒ logoff
+Ferramenta de Diagnóstico do Direct X: dxdiag
+Ferramenta para Sincornização ⇒ mobsync
+Ferramentas Administrativas: control admintools
+Ferramentas Admnistrativas ⇒ control admintools
+Findfast: findfast.cpl
+Firefox (Executar se instalado ) ⇒ firefox
+Firewall do Windows: firewall.cpl
+Fontes: control fonts
+Fonts (Pasta) ⇒ fonts
+Fonts ⇒ control fonts
+Gerenciador de Certeficados ⇒ certmgr.msc
+Gerenciador de Objetos ⇒ packager
+Gerenciamento de Discos ⇒ diskmgmt.msc
+Gerenciamento de dispositivos ⇒ devmgmt.msc
+Gerenciamento de partição de Discos ⇒ diskpart
+Gerenciamento do computador ⇒ compmgmt.msc
+Gestão de Discos: diskmgmt.msc
+Gestão do Computador: compmgmt.msc
+Gestor de Disco: diskmgmt.msc
+Gestor de Dispositivos: devmgmt.msc
+Gestor de Dispositivos: devmgmt.msc
+Gestor de Objectos - Pacote: packager
+Gestor de Partição: diskpart
+Gestor de Tarefas: taskmgr
+Gestor de Tarefas: taskmgr
+Gestor de Usuários do Wint Server (somente windows2003 server): usrmgr
+Gestor de Utilitários: utilman
+Gestor de Verificação de controladores: verifier
+Gestor do Computador: compmgmt.msc
+Group Policy Editor (XP Prof) ⇒ gpedit.msc
+Group Policy Editor (XP Prof): gpedit.msc
+HyperTerminal ⇒ hypertrm
+Iexpress Wizard ⇒ iexpress
+Iexpress Wizard: iexpress
+Impressoras e Aparelhos de Fax: control printers
+Impressoras e Faxes ⇒ control printers
+Indexing Service ⇒ ciadv.msc
+Instalador do Active Directory (somente Windows server): dcpromo
+Internet Connection Wizard ⇒ icwconn1
+Internet Explorer ⇒ iexplore
+Introdução ao Windows XP: tourstart
+IP Configuration (Atualizar DHCP & Re-Registers DNS) ⇒ ipconfig /registerdns
+IP Configuration (Modificar DHCP Class ID) ⇒ ipconfig /setclassid
+IP Configuration (Renovar Conexões ) ⇒ ipconfig /renew
+IP Configuration (Visualizar DHCP Class ID) ⇒ ipconfig /showclassid
+Janela das Ligações de Rede: ncpa.cpl
+Janela de comandos DOS ⇒ cmd
+Jogo de cartas ⇒ mshearts
+Jogo FreeCell ⇒ freecell
+Lente de Aumento: magnify
+Limpeza de Disco ⇒ cleanmgr
+Limpeza de Disco: cleanmgr
+Linha de Comando: cmd
+Logoff do Windows: logoff
+Mapa de caracteres ⇒ charmap
+Mapa de Caracteres: charmap
+Microsoft Access (Executar se instalado ) ⇒ access.cpl
+Microsoft Chat⇒ winchat
+Microsoft Excel (Executar se instalado ) ⇒ excel
+Microsoft Frontpage (Executar se instalado ) ⇒ frontpg
+Microsoft Movie Maker (Ferramenta para edição de filmes caseiros) ⇒ moviemk
+Microsoft Paint (Famoso Paint Brushe ) ⇒ mspaint
+Microsoft Powerpoint (Executar se instalado ) ⇒ powerpnt
+Microsoft Word (Executar se instalado ) ⇒ winword
+Minesweeper Game (Jogo “ Mina ”) ⇒ winmine
+Nero (Executar se instalado ) ⇒ nero
+Netmeeting (Sofwtare para comunicação, utilizado geralmente em empresas de pequeno porte ) ⇒ conf
+Nview Desktop Manager (Executar se instalado ) ⇒ nvtuicpl.cpl
+Opções de Acessibilidade: verifier
+Opções de Pasta: control folders
+Opções de telefone e modem ⇒ telephon.cpl
+Opções de Telefone e Modem: telephon.cpl
+Opções Regionais e de Idioma: intl.cpl
+Opens AC3 Filter (Executar se instalado ) ⇒ ac3filter.cpl
+Outlook Express ⇒ msimn
+Painel de Controle ⇒ control
+Painel de controle Java (Executar se instalado ) ⇒ javaws
+Painel de controle Java (Executar se instalado ) ⇒ jpicpl32.cpl
+Painel de Controlo do Direct X (se estiver instalado): directx.cpl
+Paint ⇒ pbrush
+Password Properties: password.cpl
+Pasta de impressoras ⇒ printers
+Pasta Fontes: fonts
+Pasta Impressoras:printers
+Pastas Compartilhadas: fsmgmt.msc
+Pastas Compartilhadas: fsmgmt.msc
+Performance Monitor: perfmon.msc
+Pinball Game ⇒ pinball
+Politicas de Grupo: gpedit.msc
+Propriedades da Internet ⇒ inetcpl.cpl
+Propriedades de Data e Hora: timedate.cpl
+Propriedades de data e tempo ⇒ timedate.cpl
+Propriedades de Internet: inetcpl.cpl
+Propriedades de opções de energia ⇒ powercfg.cpl
+Propriedades de Opções de Energia: powercfg.cpl
+Propriedades de pastas ⇒ control folders
+Propriedades de Rato: control mouse
+Propriedades de Rato: main.cpl
+Propriedades de senha ⇒ password.cpl
+Propriedades de Som e Dispositivos de Áudio: mmsys.cpl
+Propriedades de Teclado: control keyboard
+Propriedades de Vídeo (aparência) ⇒ control color
+Propriedades de Vídeo (com a aba Aparência já seleccionada): control color
+Propriedades de Vídeo ⇒ control desktop
+Propriedades de Vídeo ⇒ desk.cpl
+Propriedades de Vídeo: control desktop
+Propriedades de Vídeo: desk.cpl
+Propriedades do Mouse ⇒ control mouse
+Propriedades do Mouse ⇒ main.cpl
+Propriedades do Sistema: sysdm.cpl
+Propriedades do teclado ⇒ control keyboard
+Protegendo Banco de Dados de Contas do Windows XP: syskey
+Quicktime (Se instalado ) ⇒ QuickTime.cpl
+Quicktime Player (Se instalado executar o Quick Time ) ⇒ quicktimeplayer
+Real Player (Se instalado Executar o Real player ) ⇒ realplay
+Resultant Set of Policies: rsop.msc
+Scanners e Câmeras: sticpl.cpl
+Serviço de Indexação: ciadv.msc
+Serviços ⇒ services.msc
+Serviços de Componentes ⇒ dcomcnfg
+Serviços de Componentes: comexp.msc
+Serviços de Componentes: dcomcnfg
+Serviços: services.msc
+Serviços: services.msc
+Setup de Conexões de rede ⇒ netsetup.cpl
+Software para retirar Malwares (fraco) ⇒ mrt
+Solicitação do operador de armazenamento removível ⇒ ntmsoprq.msc
+Solicitações do Operador de Armazenamento Removível: ntmsoprq.msc
+Solitário (Jogo) ⇒ spider
+Som e áudio ⇒ mmsys.cpl
+SQL Client Configuration ⇒ cliconfg
+Suporte a Configurações da Internet ⇒ inetwiz
+System Configuration Editor ⇒ sysedit
+System Configuration Utility ⇒ msconfig
+System File Checker (Define o tamanho de cache): sfc /cachesize=x
+System File Checker (Limpa o cache do arquivo): sfc /purgecache
+System File Checker (Pesquisa imediatamente todos os APS a cada inicialização): sfc /scanboot
+System File Checker (Pesquisa imediatamente todos os APS): sfc /scannow
+System File Checker (Restaura a pesquisa à configuração padrão): sfc /revert
+System File Checker Utility (Purge File Cache) ⇒ sfc /purgecache
+System File Checker Utility (Return to Default Setting) ⇒ sfc /revert
+System File Checker Utility (Scan Immediately) ⇒ sfc /scannow
+System File Checker Utility (Scan On Every Boot) ⇒ sfc /scanboot
+System File Checker Utility (Scan Once At Next Boot) ⇒ sfc /scanonce
+System File Checker Utility (Set Cache Size to size x) ⇒ sfc /cachesize=x
+System Information ⇒ msinfo32
+System Properties ⇒ sysdm.cpl
+Tarefas Agendadas ⇒ control schedtasks
+Tarefas Agendadas: control schedtasks
+Task Manager ⇒ taskmgr
+TCP Tester ⇒ tcptest
+Teclado virtual ⇒ osk
+Telnet Client(Executar Telnet) ⇒ telnet
+Tweak UI (Se instalado ) ⇒ tweakui
+User Account Management (Controlar Contas de usuário ) ⇒ nusrmgr.cpl
+Usuários e Computadores do Active Directory ⇒ dsa.msc
+Usuários e Grupos locais ⇒ lusrmgr.msc
+Usuários e Grupos Locais: lusrmgr.msc
+Utilitário de Configuração do Sistema: msconfig
+Utilitário de Rede para Clientes do SQL Server: cliconfg
+Utility Manager ⇒ utilman
+Utilizadores Locais e Grupos: lusrmgr.msc
+Verificação de assinatura de arquivo ⇒ sigverif
+Verificação de Assinatura de Arquivo:sigverif
+Visualizador da área de armazenamento ⇒ clipbrd
+Visualizador da Área de Transferência: clipbrd
+Visualizador de Eventos: eventvwr.msc
+Visualizador de Eventos: eventvwr.msc
+Windows Address Book (Serviço para guardar lista de contatos ) ⇒ wab
+Windows Address Book Import Utility (Serviço para importar lista de contatos) ⇒ wabmig
+Windows Backup Utility (Fazer Backup ) ⇒ ntbackup
+Windows Explorer (Executar Explorer) ⇒ explorer
+Windows Firewall (Executar Firewall) ⇒ firewall.cpl
+Windows Magnifier ⇒ magnify
+Windows Management Infrastructure ⇒ wmimgmt.msc
+Windows Management Infrastructure: wmimgmt.msc
+Windows Media Player (Executar Media player ) ⇒ wmplayer
+Windows Messenger (Executar MSN) ⇒ msmsgs
+Windows Picture Import Wizard (Precisa estar com a camera instalada ) ⇒ wiaacmgr
+Windows System Security Tool (ferramenta de segurança do windows ) ⇒ syskey
+Windows Update Launches (ver updates ) ⇒ wupdmgr
+Windows Version (Ver a versão do Windows ) ⇒ winver
+Windows XP Tour Wizard (Tour Windows) ⇒ tourstart
+Wordpad ⇒ write
